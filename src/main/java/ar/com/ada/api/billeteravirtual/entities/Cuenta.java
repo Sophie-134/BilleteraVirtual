357e1d88d9a7f6db6c302226a680d2c2067fee17
Sophie-134 <sofiammendizabal@gmail.com>
2020-07-02 00:30:14 -0300

    Se crea el metodo enviar saldo encargada de realizar transacciones las de entrada y salida en las cuentas de los usuarios. Faltan agregar los controllers correspondientes a billeteraController

--- src/main/java/ar/com/ada/api/billeteravirtual/entities/Cuenta.java
+++ src/main/java/ar/com/ada/api/billeteravirtual/entities/Cuenta.java
@@ -18,7 +18,7 @@ public class Cuenta {
     @JoinColumn(name = "billetera_id", referencedColumnName = "billetera_id")
     private Billetera billetera;
 
-    @OneToMany(mappedBy ="cuenta", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cuenta", cascade = CascadeType.ALL)
     private List<Transaccion> transacciones = new ArrayList<>();
 
     public Integer getCuentaId() {
@@ -51,7 +51,7 @@ public class Cuenta {
 
     public void setBilletera(Billetera billetera) {
         this.billetera = billetera;
-        //this.billetera.getCuentas().add(this);
+        // this.billetera.getCuentas().add(this);
     }
 
     public List<Transaccion> getTransacciones() {
@@ -61,9 +61,46 @@ public class Cuenta {
     public void setTransacciones(List<Transaccion> transacciones) {
         this.transacciones = transacciones;
     }
-//la relacion bidireccional se hace a travez de un metodo(agregarAlgo) q lo agrega a la lista
+
+    // la relacion bidireccional se hace a travez de un metodo(agregarAlgo) q lo
+    // agrega a la lista
     public void agregarTransacciones(Transaccion transaccion) {
         this.transacciones.add(transaccion);
         transaccion.setCuenta(this);
+
+        BigDecimal saldoActual = this.getSaldo();
+
+        if (transaccion.getTipoOperacion().equals(1)) {
+            BigDecimal saldoSumado = saldoActual.add(saldo);
+            this.setSaldo(saldoSumado);
+        } else {
+            BigDecimal saldoSumado = saldoActual.subtract(saldo);
+            this.setSaldo(saldoSumado);
+        }
     }
+
+    public Transaccion generarTransaccion(String conceptoOperacion, String detalle, BigDecimal importe,
+            Integer tipoOperacion) {
+
+        Transaccion transaccion = new Transaccion();
+
+        transaccion.setFecha(new Date());
+        transaccion.setEstadoId(2); // 2=aprobado 0=pendiente -1=rechazada
+        transaccion.setImporte(importe);
+        transaccion.setMoneda(moneda);
+        transaccion.setTipoOperacion(tipoOperacion); // 1=entrada, 0 =salida
+        transaccion.setConceptoOperacion(conceptoOperacion);
+        transaccion.setDetalle(detalle);
+
+        if (transaccion.getTipoOperacion() == 1) {// si es de entrada
+
+            transaccion.setaUsuarioId(billetera.getPersona().getUsuario().getUsuarioId());
+            transaccion.setaCuentaId(this.getCuentaId());
+        } else { // de salida
+            transaccion.setDeUsuarioId(billetera.getPersona().getUsuario().getUsuarioId());
+            transaccion.setDeCuentaId(this.getCuentaId());
+        }
+        return transaccion;
+    }
+
 }
